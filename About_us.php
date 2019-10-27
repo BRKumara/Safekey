5ce0ebc392ff23e7360366e4669e782da2badc3d
Tharaka Udayanga <42183861+tharudaya@users.noreply.github.com>
2019-10-27 16:02:45 +0530

    updated about us page
    Title should be match to the page.
    Comments should be placed in correct positions

--- About_us.php
+++ About_us.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap Simple Login Form</title>
+        <title>Abou Us</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 
-        <!--        footer-->
+       
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -17,29 +17,24 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     </head>
+    
     <body>
+        
         <div>
-
-
             <nav class="navbar navbar-inverse" style="opacity: 0.8;">
                 <div style="color:white; position: absolute; left:1;" >
                     <h4>Safe Key Home Security - About Us</h4>
                 </div>
-
             </nav>
         </div>
-
-
-
-
     </div>
+    
 </body>
 
-
+<!-- footer -->
 <?php
 include 'Include_Footer.php';
 ?>
-
 
 
 </html>                                		                            
